855bcbf2c78bd0808b715d26d4a17d762aee4426
github username <github email address>
2021-02-17 17:40:01 -0500

    serializers added

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -2,5 +2,6 @@ class Queen < ApplicationRecord
     has_many :contracts 
     has_many :teams, through: :contracts 
     belongs_to :country
+    has_many :comments 
 
 end
--- app/serializers/queen_serializer.rb
+++ app/serializers/queen_serializer.rb
@@ -1,4 +1,5 @@
 class QueenSerializer < ActiveModel::Serializer
   attributes :id, :name, :season, :grid_image, :freeze_frame, :gif, :quote, :instagram, :twitter, :country_id
   belongs_to :country
+  belongs_to :comments
 end
